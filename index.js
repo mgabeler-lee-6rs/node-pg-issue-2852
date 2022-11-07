28a21e6b859520cb9ebbc2f89d73c08bda9327f5
Matthew Gabeler-Lee <mgabeler-lee@6river.com>
2022-11-07 13:31:44 -0500

    fix: demo one way to kinda make it work

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 // make sure you set PGHOST, etc.
 
+const assert = require("assert");
 const { Pool } = require("pg");
 
 async function main() {
   const pool = new Pool({});
-  pool.on("error", (err, client) => {
-    console.error("Got an error", { err });
+  let errorCount = 0;
+  const errorLogger = (err) => {
+    ++errorCount;
+    console.error("Got an error", { err: err.message });
+  };
+  pool.on("error", errorLogger);
+  // this is a nice way to not have to repeat the per-acquire error logging
+  // logic, but there's no clean way to remove this error listener on each
+  // release.
+  pool.on("acquire", (client) => {
+    client.removeListener("error", errorLogger);
+    client.on("error", errorLogger);
   });
 
   await mockWork(pool, false);
+  assert.equal(errorCount, 0);
   await mockWork(pool, true);
+	// we get two ambient errors most of the time here: 'terminating connection
+	// due to idle-in-transaction timeout' and 'Connection terminated
+	// unexpectedly'
+  assert.equal(errorCount, 2);
+  errorCount = 0;
+  await mockWork(pool, true);
+  assert.equal(errorCount, 2);
 }
 
 async function mockWork(pool, timeout) {
   const client = await pool.connect();
   // timeout connection after 5 seconds idle in transaction
-  await client.query("SET SESSION idle_in_transaction_session_timeout = 5000");
+  await client.query("SET SESSION idle_in_transaction_session_timeout = 1000");
 
   console.log("Starting transaction");
-  // mock: a transaction that might pause too long and wants to catch the idle error
   await client.query("BEGIN");
   let doRollback = true;
   try {
@@ -26,7 +44,7 @@ async function mockWork(pool, timeout) {
     // mock some work
     await client.query("SELECT 1");
     console.log("Mocking a delay");
-    const delay = timeout ? 6000 : 4000;
+    const delay = timeout ? 1500 : 500;
     // mock making some other network call that takes "too long"
     await new Promise((r) => setTimeout(r, delay));
     doRollback = false;
